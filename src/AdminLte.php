0d38e8a856cf62a8e7605edfb959f5ab535016b9
Maxim Holodilin <maxim@nyx-tech.com>
2020-04-19 06:26:57 +0300

    Fisrt upload

---
+++ src/AdminLte.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace UserFrosting\Sprinkle\AdminLte;
+
+use UserFrosting\System\Sprinkle\Sprinkle;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Bootstrapper class for the 'AdminLte' sprinkle.
+ */
+class AdminLte extends Sprinkle
+{
+
+}
